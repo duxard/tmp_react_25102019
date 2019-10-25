ae374709f5096353b81ebbb9de61f7ee4a70b506
Aleksandr <duxard@gmail.com>
2019-10-25 11:55:23 -0400

    setup app

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/wrapper.scss';
+
+const Wrapper = () => {
+  return (
+    <div>
+       <h1>wrapper</h1>
+       <a href="https://en.wikipedia.org/wiki/Wiki" target="_blank">Go to wiki...</a>
+    </div>
+  );
+}
+
+export default Wrapper;
