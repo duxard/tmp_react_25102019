7d814d76060e3168b9486415228c8c61603ef4c8
Aleksandr <duxard@gmail.com>
2019-11-22 11:11:05 -0500

    init 2

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './styles/wrapper.scss';
 
 const Wrapper = () => {
   return (
     <div>
-       <h1>wrapper</h1>
-       <a href="https://en.wikipedia.org/wiki/Wiki" target="_blank">Go to wiki...</a>
+       <h3>Home page</h3>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem doloribus non aut repellendus sequi distinctio sit exercitationem tempore, autem atque vero. Praesentium animi, nihil architecto velit. Vitae in temporibus recusandae qui architecto quidem fugiat non placeat quod nisi. Ad provident explicabo in esse! Aut eius ipsam inventore ipsum sed quae nesciunt autem, eveniet, veniam corporis totam, deleniti quasi? Consectetur quod dolores iure maiores exercitationem soluta modi, necessitatibus delectus itaque nemo rerum, vero ad vel odio quam, laborum fuga minima odit minus! Iure animi nisi iusto illum totam, dolores natus est explicabo reiciendis omnis aliquid suscipit ad, modi dolor inventore commodi ea cum odit. Animi, maxime saepe labore corrupti enim, itaque quo atque aliquid magnam, natus minima impedit et! Ipsum ipsa vel voluptas! Accusantium reiciendis magni dolor sed, quo impedit. Distinctio inventore in beatae exercitationem earum harum error maiores nam, sapiente fugit eius ex vero suscipit. Ut at maxime delectus quo totam rem, reprehenderit et earum ducimus repellendus. Quaerat animi eum ut obcaecati amet blanditiis reiciendis, doloribus quod itaque id doloremque, modi vero veritatis commodi distinctio ab quia laudantium consectetur, ipsum pariatur ducimus eligendi. Nisi veritatis possimus molestiae eos voluptatum voluptas dolores error sunt numquam! Officiis, inventore distinctio alias commodi optio vero nihil, architecto minima, nam non saepe ut deserunt fugit minus, illum quidem deleniti magni! Laboriosam ad nostrum sequi a sint qui, eius beatae quam inventore similique velit non repudiandae architecto distinctio tenetur doloribus rerum facere. Iure dignissimos molestiae nihil, fugit neque facilis totam non blanditiis consectetur aut alias dolor assumenda error, quam harum. At odio, temporibus quam beatae minima hic consectetur! Voluptatem officia a, earum, rem atque eos? Soluta impedit repudiandae voluptatem aut totam eligendi ea, odit inventore, earum facilis placeat nobis, tenetur sapiente quia perferendis magni, quidem neque ad dolores laborum autem hic omnis! In, dolor blanditiis ut?</p>
     </div>
   );
 }
